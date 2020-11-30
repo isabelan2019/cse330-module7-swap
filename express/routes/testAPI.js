11a5412088397525c4a90001c1fa1a5fb333da94
Claire <claire.irawan@wustl.edu>
2020-11-29 22:12:33 -0600

    api is working properly

---
+++ express/routes/testAPI.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", function(req, res, next) {
+    res.send("API is working properly");
+});
+
+module.exports = router;
+// from https://www.freecodecamp.org/news/create-a-react-frontend-a-node-express-backend-and-connect-them-together-c5798926047c/
