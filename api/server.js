2bdcae10fbdf47159edce6b5ee39469e43901660
Claire <claire.irawan@wustl.edu>
2020-12-02 12:23:46 -0600

    mongodb dependency

--- api/server.js
+++ api/server.js
@@ -18,8 +18,15 @@ const assert = require('assert');
 // Connection URL
 const url = 'mongodb://mongodb:mongodb@localhost:27017/swap?authSource=admin';
 
+//Database Name 
+const dbName = 'swap';
+
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, client) {
   assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+
   client.close();
 });
