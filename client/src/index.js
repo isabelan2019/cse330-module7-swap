1bc43b8198cc75caf22bf0ee835609a04e989db0
Isabel An <isabel.an@wustl.edu>
2020-12-01 23:43:56 -0600

    started building front end

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,8 @@ import './index.css';
 import Axios from "axios";
 
 
-function HelloWorld(){
+function App(){
   //connecting server to backend through axios: https://medium.com/better-programming/connect-your-express-and-react-applications-using-axios-c35723b6d667 
-
   Axios({
     method: "GET",
     url: "http://localhost:5000/",
@@ -18,16 +17,64 @@ function HelloWorld(){
   });
 
   return(
-    <p> Hello World </p>
+    <div>
+      <NavBar />
+      <LogIn />
+    </div>
   );
 }
 
+function NavBar(){
+  return(
+    <nav>
+      <button> Checkout</button>
+      <button> Transactions </button>
+      <button> Inventory Log</button>
+    </nav>
+  );
+}
+
+class LogIn extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      username:"",
+      password:""
+    }
+  }
+  render(){
+    return(
+      <form>
+        <label for="username"> Username: </label>
+        <input type="text" value={this.state.username}/>
+        <label for="password"> Password: </label>
+        <input type="password" value={this.state.password}/>
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  } 
+}
+
+class ItemInventory extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <table>
+        <tbody>
+          <th> Item Type </th>
+          <th> Quantity </th> 
+        </tbody>
+      </table>
+    )
+  }
+}
+
+
+
 
 ReactDOM.render(
-  <HelloWorld/>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
