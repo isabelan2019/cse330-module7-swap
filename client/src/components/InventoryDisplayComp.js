614d23733808b503a039264353eeb4f28c673a0e
Isabel An <isabel.an@wustl.edu>
2020-12-09 21:53:18 -0600

    testing

--- client/src/components/InventoryDisplayComp.js
+++ client/src/components/InventoryDisplayComp.js
@@ -13,111 +13,6 @@ class InventoryPage extends React.Component {
   }
 }
 
-class InventoryDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inventoryData: [],
-    };
-    this.getData = this.getData.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-    this.editQuantity = this.editQuantity.bind(this);
-  }
-
-  componentDidMount() {
-    this.getData();
-  }
-
-  getData() {
-    axios.get("http://localhost:5000/getAllInventory").then((res) => {
-      let allCategories = [];
-      const data = res.data;
-      console.log(data);
-      for (let i in data) {
-        allCategories.push(data[i]);
-        let allItemTypes = [];
-        for (let j in data[i].itemTypes) {
-          allItemTypes.push(data[i].itemTypes[j]);
-        }
-        allCategories[i].itemTypes = allItemTypes;
-      }
-      this.setState({ inventoryData: allCategories });
-    });
-  }
-
-  deleteItem(event) {
-    event.preventDefault();
-    const itemID = event.target.parentNode.parentNode.id;
-    const categoryID =
-      event.target.parentNode.parentNode.parentNode.parentNode.parentNode.id;
-    const deletedObject = {
-      categoryID: categoryID,
-      itemID: itemID,
-    };
-    axios
-      .post("http://localhost:5000/deleteInventoryItem", deletedObject)
-      .then((res) => console.log(res.data));
-  }
-
-  editQuantity(event) {
-    event.preventDefault();
-    const itemID = event.target.parentNode.parentNode.id;
-    const categoryID =
-      event.target.parentNode.parentNode.parentNode.parentNode.parentNode.id;
-    const editObject = {
-      categoryID: categoryID,
-      itemID: itemID,
-    };
-    axios
-      .post("http://localhost:5000/editInventoryQuantity", editObject)
-      .then((res) => console.log(res.data));
-  }
-
-  render() {
-    if (!this.state.inventoryData) {
-      return null;
-    }
-    return (
-      <div>
-        <ul>
-          {this.state.inventoryData.map((data) => (
-            <li key={data._id} id={data._id}>
-              <h2>{data.category}</h2>
-              <table>
-                <tbody>
-                  <tr>
-                    <th> Item Name </th>
-                    <th> Quantity</th>
-                    <th> Actions</th>
-                  </tr>
-                  {data.itemTypes.map((item) => (
-                    <tr id={item._id} key={item._id}>
-                      <td> {item.itemName} </td>
-                      <td> {item.quantity} </td>
-                      <td>
-                        <input
-                          type="button"
-                          onClick={this.editQuantity}
-                          value="Edit Quantity"
-                        />
-                        <input
-                          type="button"
-                          onClick={this.deleteItem}
-                          value="Delete Item"
-                        />
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
-
 class InventoryCategory extends React.Component {
   constructor(props) {
     super(props);
@@ -144,10 +39,10 @@ class InventoryCategory extends React.Component {
       .post("http://localhost:5000/addInventoryCategory", categoryObj)
       .then((res) => {
         console.log(res.data);
-        this.setState((prev) => {
-          console.log(prev);
-          return { category: prev.categories.push(res) };
-        });
+        // this.setState((prev) => {
+        //   console.log(prev);
+        //   return { category: prev.categories.push(res) };
+        // });
       });
   }
 
@@ -272,6 +167,117 @@ class InventoryForm extends React.Component {
         </label>
         <input type="submit" value="Add" />
       </form>
+    );
+  }
+}
+
+class InventoryDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inventoryData: [],
+    };
+    this.getData = this.getData.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.editQuantity = this.editQuantity.bind(this);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios.get("http://localhost:5000/getAllInventory").then((res) => {
+      let allCategories = [];
+      const data = res.data;
+      console.log(data);
+      for (let i in data) {
+        allCategories.push(data[i]);
+        let allItemTypes = [];
+        for (let j in data[i].itemTypes) {
+          allItemTypes.push(data[i].itemTypes[j]);
+        }
+        allCategories[i].itemTypes = allItemTypes;
+      }
+      this.setState({ inventoryData: allCategories });
+    });
+  }
+
+  getChildInfo(childData) {
+    this.setState({ data: childData });
+  }
+
+  deleteItem(event) {
+    event.preventDefault();
+    const itemID = event.target.parentNode.parentNode.id;
+    const categoryID =
+      event.target.parentNode.parentNode.parentNode.parentNode.parentNode.id;
+    const deletedObject = {
+      categoryID: categoryID,
+      itemID: itemID,
+    };
+
+    axios
+      .post("http://localhost:5000/deleteInventoryItem", deletedObject)
+      .then((res) => console.log(res.data));
+  }
+
+  editQuantity(event) {
+    event.preventDefault();
+    const itemID = event.target.parentNode.parentNode.id;
+    const categoryID =
+      event.target.parentNode.parentNode.parentNode.parentNode.parentNode.id;
+    const editObject = {
+      categoryID: categoryID,
+      itemID: itemID,
+    };
+
+    axios
+      .post("http://localhost:5000/editInventoryQuantity", editObject)
+      .then((res) => console.log(res.data));
+  }
+
+  render() {
+    if (!this.state.inventoryData) {
+      return null;
+    }
+    return (
+      <div>
+        <ul>
+          {this.state.inventoryData.map((data) => (
+            <li key={data._id} id={data._id}>
+              <h2>{data.category}</h2>
+              <table>
+                <tbody>
+                  <tr>
+                    <th> Item Name </th>
+                    <th> Quantity</th>
+                    <th> Actions</th>
+                  </tr>
+                  {data.itemTypes.map((item) => (
+                    <tr id={item._id} key={item._id}>
+                      <td> {item.itemName} </td>
+                      <td> {item.quantity} </td>
+                      <td>
+                        <input
+                          type="button"
+                          onClick={this.editQuantity}
+                          value="Edit Quantity"
+                        />
+                        <input
+                          type="button"
+                          onClick={this.deleteItem}
+                          value="Delete Item"
+                        />
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
