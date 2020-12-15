3afedda2ed85190e626a6bc272c10eca5222e82f
Isabel An <isabel.an@wustl.edu>
2020-12-15 15:21:08 -0600

    finished

--- client/src/components/NavBarComp.js
+++ client/src/components/NavBarComp.js
@@ -9,37 +9,41 @@ import TransactionsLog from "./TransactionsLogComp";
 import InventoryDisplay from "./InventoryDisplayComp";
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
-
-
 class NavBar extends React.Component {
-    // constructor(props){
-    //   super(props);
-    // }
-    render() {
-      // const hasSession = sessionStorage.getItem("username");
-      return (
-        <div>
-          <Router>
-            <nav id="employeeNav">
-              {/* <button>
+  // constructor(props){
+  //   super(props);
+  // }
+  render() {
+    // const hasSession = sessionStorage.getItem("username");
+    return (
+      <div>
+        <Router>
+          <nav id="employeeNav">
+            {/* <button>
                 <Link to="/">Home</Link>
               </button> */}
-              <br/>
-              <Link class="link"  to="/checkout">Checkout</Link>
-              <Link class="link"  to="/transactions">Transactions</Link>
-              <Link class="link"  to="/inventory">Inventory Log</Link>
-              <br/><br/>
-            </nav>
-            <Switch>
-              <Route path="/checkout" component={CustomerCheckout} />
-              <Route path="/transactions" component={TransactionsLog} />
-              <Route path="/inventory" component={InventoryDisplay} />
-            </Switch>
-          </Router>
-          
-        </div>
-      );
-    }
+            <br />
+            <Link className="link" to="/checkout">
+              Checkout
+            </Link>
+            <Link className="link" to="/transactions">
+              Transactions
+            </Link>
+            <Link className="link" to="/inventory">
+              Inventory Log
+            </Link>
+            <br />
+            <br />
+          </nav>
+          <Switch>
+            <Route path="/checkout" component={CustomerCheckout} />
+            <Route path="/transactions" component={TransactionsLog} />
+            <Route path="/inventory" component={InventoryDisplay} />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
+}
 
-  export default NavBar;
+export default NavBar;
